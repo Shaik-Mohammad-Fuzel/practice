3ab82eae82caf1fcf4bf8f7252e07672920311fe
Shaik-Mohammad-Fuzel <mohammadfuzel24@gmail.com>
2024-12-20 10:33:48 +0530

    initial commit

--- basicWordCount.py
+++ basicWordCount.py
@@ -1,12 +1,13 @@
-text="i like red color , but my father doesnot like red color , he likes yellow in color"
+text="i like red color, but my father doesnot like red color , he likes yellow in color"
 a={}
 def wordcount(text):
-    w=text.split()
+    w=text.strip('.').split()
     for i in w:
-        if i not in a:
-            a[i]=1
+        w=i.strip(',')
+        if w not in a:
+            a[w]=1
         else :
-            a[i]+=1
+            a[w]+=1
     print(a)
 wordcount(text)
             
